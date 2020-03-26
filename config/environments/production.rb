34c119364f1b5fbbc9317156c644c3a60113bfb0
tegnike <user@mbp.local>
2020-03-26 17:15:01 +0100

    off CRSF

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # CRSF off
+  config.action_controller.allow_forgery_protection = false
 end
